6f70f4611bb8d7aa5b629ff6b0af980df753b8a5
Adam Szczombrowski <adam.szczombrowski@gmail.com>
2016-05-24 11:33:22 +0200

    edit home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def welcome
-    @articles = Article.all.order('created_at desc')
+    @articles = Article.all.order('created_at desc').limit(4)
+    @projects = Project.all.order('created_at desc').limit(4)
   end
 
   def contact
