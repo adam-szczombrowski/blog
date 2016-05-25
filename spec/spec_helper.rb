0a19c4ef0c01f5a2260abe16bfcb7946827c2a9b
Adam Szczombrowski <adam.szczombrowski@gmail.com>
2016-05-25 11:52:20 +0200

    add configuration to rails and spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'support/controller_helpers'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -34,6 +35,9 @@ RSpec.configure do |config|
   config.before(:all) do
     FactoryGirl.reload
   end
+
+  #config.include Devise::TestHelpers, :type => :controller
+  #config.include ControllerHelpers, :type => :controller
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
