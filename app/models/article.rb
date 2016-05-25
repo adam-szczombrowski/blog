c23170e14ad766db2c1ba13a3526ff7f738086b5
Adam Szczombrowski <adam.szczombrowski@gmail.com>
2016-05-25 07:56:28 +0200

    add models validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  validates :name, :content, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  validates :name, :description, :link, presence: true
+  validates :name, uniqueness: true
 end
